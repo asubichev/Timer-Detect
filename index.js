78e2e7482026b6fe4671414a9115c93801e9ad55
asubichev <asubichev@gmail.com>
2022-10-25 00:06:44 -0500

    trying to get a scraper to work

---
+++ index.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+	product: 'firefox'
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.bannerbear.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})()
